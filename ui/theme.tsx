53f15629b26ee7f66c8c6a571263e6d51f9f1bb5
RayDarar <dfqgth400@gmail.com>
2024-07-09 00:31:48 +0500

    correct icons

--- ui/theme.tsx
+++ ui/theme.tsx
@@ -7,6 +7,22 @@ type SanaTheme = {
   fonts: (typeof MD3LightTheme)["fonts"];
   colors: (typeof MD3LightTheme)["colors"] & {
     text: string;
+    text80: string;
+    text60: string;
+    text30: string;
+    system: string;
+    system80: string;
+    system60: string;
+    system30: string;
+    system10: string;
+    status: {
+      dangerous: string;
+      bad: string;
+      good: string;
+    };
+    backgroundExtra: string;
+    activePrimary: string;
+    activeSecondary: string;
   };
 };
 
@@ -24,14 +40,48 @@ export const useRootTheme = ({
         ...MD3LightTheme,
         colors: {
           ...MD3LightTheme.colors,
-          text: "#333333",
+          text: "#04021D",
+          text80: "#36354A",
+          text60: "#686777",
+          text30: "#B3B5BD",
+          system: "#554AF0",
+          system80: "#776EF3",
+          system60: "#9992F6",
+          system30: "#CCC9FB",
+          system10: "#EEEDFE",
+          status: {
+            dangerous: "#FF0000",
+            bad: "#FF6B00",
+            good: "#15AA2C",
+          },
+          background: "#FFFFFF",
+          backgroundExtra: "#F8F8FA",
+          activePrimary: "#2113E8",
+          activeSecondary: "#6D61F2",
         },
       }
     : {
         ...MD3DarkTheme,
         colors: {
           ...MD3DarkTheme.colors,
-          text: "#ffffff",
+          text: "#04021D",
+          text80: "#36354A",
+          text60: "#686777",
+          text30: "#B3B5BD",
+          system: "#554AF0",
+          system80: "#776EF3",
+          system60: "#9992F6",
+          system30: "#CCC9FB",
+          system10: "#EEEDFE",
+          status: {
+            dangerous: "#FF0000",
+            bad: "#FF6B00",
+            good: "#15AA2C",
+          },
+          background: "#FFFFFF",
+          backgroundExtra: "#F8F8FA",
+          activePrimary: "#2113E8",
+          activeSecondary: "#6D61F2",
         },
       };
 };
